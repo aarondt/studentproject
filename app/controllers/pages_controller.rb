7311cd7974a953e516f874d8ed69b205d2f42ac6
aadt91 <aaron.tal@hotmail.de>
2017-11-22 16:19:27 +0000

    major changes to layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
-    
+before_action :authenticate_user!, :except => [:start]
+
     def home
         @users = User.all
         @current_user = current_user
     end
-
+    
+    def start
+        render layout: false
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'pages#home'
+  root 'pages#start'
   resources :profiles
   get '/about', to: 'pages#about'
-  
+  get '/home', to: 'pages#home'
   
   #root :to => 'rooms#index'
   resources :rooms
